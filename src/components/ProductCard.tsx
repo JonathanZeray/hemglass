ebf6f98b83f495b78580f26d6d1d1c8ebd65323a
Jonathan Zeray <jonathan.zeray@hotmail.com>
2025-04-04 00:34:09 +0200

    fix: align cart button across uneven product titles, adjust product image size.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -18,9 +18,9 @@ const ProductCard = ({
   const formattedPrice = `${price / 100} kr`;
 
   return (
-    <div className="bg-hemglass-white flex flex-col items-center gap-2 text-center">
+    <div className="bg-hemglass-white flex flex-col h-full items-center gap-2 text-center">
       <div
-        className="shadow p-4 flex flex-col items-center space-y-2 text-center rounded-xl bg-cover bg-center bg-no-repeat bg-hemglass-lightblue min-h-[300px] flex justify-center"
+        className="shadow p-4 flex flex-col items-center space-y-2 text-center rounded-xl bg-cover bg-center bg-no-repeat bg-hemglass-lightblue min-h-[325px] flex justify-center"
         style={{
           backgroundImage: backgroundImageUrl
             ? `url(${backgroundImageUrl})`
@@ -30,7 +30,7 @@ const ProductCard = ({
         <img
           src={imageUrl}
           alt={name}
-          className="w-full aspect-[4/3] object-contain rounded-xl transition-transform duration-300 hover:scale-105"
+          className="w-full aspect-[2/2] object-contain rounded-xl transition-transform duration-300 hover:scale-105"
         />
       </div>
       <h2 className="text-3xl font-burbank text-hemglass-darkblue -mb-2">
@@ -44,7 +44,7 @@ const ProductCard = ({
       </p>
       <button
         onClick={onAddToCart}
-        className="bg-hemglass-darkblue text-hemglass-white font-bariol text-base sm:text-sm px-4 sm:px-2 py-2 sm:py-1 rounded-full hover:brightness-110 transition"
+        className="mt-auto bg-hemglass-darkblue text-hemglass-white font-bariol text-base sm:text-sm px-4 sm:px-2 py-2 sm:py-1 rounded-full hover:brightness-110 transition"
       >
         Lägg i varukorgen
       </button>
